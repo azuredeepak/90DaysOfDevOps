a85c6d6d63401cd7ab340b2d321d007ed4696340
Deepak Kumar (LTIMindtree Limited) <v-deepkumar@microsoft.com>
2026-02-07 20:37:45 -0800

    update new

---
+++ 2026/week3/shellscriptargument.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ] # dolar hash calculates total no . of arguments.
+then
+echo "please pass an argument"
+echo "echo i ss prakar likho budbak: If _ file exlslts.sh < file_path>" 
+exit 1
+fi
+
+
+
